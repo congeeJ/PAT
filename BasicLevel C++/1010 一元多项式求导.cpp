7044fac0204473f8f26babb59516a8a64a5072f4
congeeJ <72962397+congeeJ@users.noreply.github.com>
2021-01-22 10:25:43 +0800

    对问题进行猜测
    while(cin)既可以一次输入一次运行，也可以一次输入多个再统一运行

--- BasicLevel C++/1010 一元多项式求导.cpp
+++ BasicLevel C++/1010 一元多项式求导.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main() {
     int a, b, flag = 0;
-    while (cin >> a >> b) {
+    while (cin >> a >> b) {                     //此处应该是既可以输入一次运行一次，也可以一次输入多个，再一次运行输出
         if (b != 0) {
             if (flag == 1) cout << " ";
             cout << a * b << " " << b - 1;
